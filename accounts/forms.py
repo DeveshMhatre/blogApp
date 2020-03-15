e05130ae078e91714f37d96c71565d6549ce259b
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2020-03-15 21:05:35 +0530

    Add login and logout views and pages
    Added login_user and logout_user views to accounts app.
    Made the necessary changes to the corresponding templates.
    (Pending) Error handling in register and login pages.

--- accounts/forms.py
+++ accounts/forms.py
@@ -43,3 +43,21 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        label='Username',
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Enter username'
+            }
+        )
+    )
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Enter password'
+            }
+        )
+    )
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm
 
 def register_user(request):
     if request.method == 'POST':
@@ -17,3 +18,26 @@ def register_user(request):
     }
 
     return render(request, 'accounts/register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user != None:
+                login(request, user=user)
+                return redirect('blog:articles')
+    else:
+        form = LoginForm()
+    
+    context = {
+        'form': form
+    }
+    
+    return render(request, 'accounts/login.html', context)
+
+def logout_user(request):
+    logout(request)
+    return redirect('accounts:login')
