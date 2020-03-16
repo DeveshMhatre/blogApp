3548afcc18b3370d2e84ba8ebc5ca1b7a44ac432
Devesh Mhatre <deveshmhtr.mhatre92@gmail.com>
2020-03-16 13:50:31 +0530

    Add error handling to all the forms
    Added error handling to login and register forms.
    Put restrictions on annonymous users from accessing homepage.
    Put restrictions on logged users from accessing login and register forms.
    (Pending) Styling the login and register forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from django.contrib.auth.models import User
 
 from .forms import RegisterForm, LoginForm
 
 def register_user(request):
+    if request.user.is_authenticated:
+        messages.info(request, 'You are already logged in!')
+        return redirect('blog:articles')
+
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            if user != None:
+                login(request, user=user)
+            messages.success(request, 'User registered successfully!')
             return redirect('blog:articles')
+        else:
+            messages.warning(request, 'Please correct the errors below:')
     else:
         form = RegisterForm()
     
@@ -20,6 +33,10 @@ def register_user(request):
     return render(request, 'accounts/register.html', context)
 
 def login_user(request):
+    if request.user.is_authenticated:
+        messages.info(request, 'You are already logged in!')
+        return redirect('blog:articles')
+
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -28,7 +45,10 @@ def login_user(request):
             user = authenticate(request, username=username, password=password)
             if user != None:
                 login(request, user=user)
+                messages.success(request, 'Signed in successfully')
                 return redirect('blog:articles')
+            else:
+                messages.warning(request, 'The Username or Password is incorrect. Please try again.')
     else:
         form = LoginForm()
     
@@ -40,4 +60,5 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
+    messages.info(request, 'You successfully logged out.')
     return redirect('accounts:login')
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 
 from .models import Article
 
+@login_required(login_url='accounts:login')
 def article_list(request):
     latest_article_list = Article.objects.order_by('-pub_date')[:5]
     context = {
@@ -9,6 +11,7 @@ def article_list(request):
     }
     return render(request, 'blog/article_list.html', context)
 
+@login_required(login_url='accounts:login')
 def article_detail(request, article_id):
     article = get_object_or_404(Article, pk=article_id)
     context = {
